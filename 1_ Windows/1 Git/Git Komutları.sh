cc15349a471d1e02ad82340405f708a7f2c7ca5f
Hamit_Mizrak <hamitmizrak@gmail.com>
2024-06-30 18:48:06 +0300

    readme

--- 1_ Windows/1 Git/Git Komutları.sh
+++ 1_ Windows/1 Git/Git Komutları.sh
@@ -281,16 +281,32 @@ GITHUB
 https://github.com/settings/keys
 
 
-
-
-
 ##############################################################
 ##############################################################
 -- 
+# Senior Devops
+
+## Tech
+> Git
+- GitHub
+- GitLab
+- **Docker**
+---
+````sh
+ssh-keygen -t rsa -b 4096 -C "hamitmizrak@gmail.com"
+
+Windows =>
+cat /c/Users/Hamit_Mizrak/.ssh/id_rsa.pub
+
+Linux =>
+cat ~/.ssh/id_rsa.pub
+
+ssh -T git@github.com
 
 
-
-
+GITHUB
+https://github.com/settings/keys
+```
 
 ##############################################################
 ##############################################################
